37846ad4c62d1b915bcd06dfec22015145327b1d
Tharun_Ninja <tharunharish84@gmail.com>
2021-12-23 11:38:33 +0300

    Changing button type

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
+// import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <Button>This should work</Button>
+      <a href='#' className='btn btn-warning m-5 ps-5 pe-5 rounded-pill'>This should work</a>
     </div>
   );
 }
